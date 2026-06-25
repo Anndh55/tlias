8fe8a89b8c43bc255b15eeea68f2d2cae8e0fbc3
你的GitHub用户名 <GitHub>
2026-06-25 10:11:38 +0800

    第1课：添加 /hello 接口

--- src/main/java/com/example/hello/HelloApplication.java
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -3,13 +3,21 @@ package com.example.hello;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @MapperScan("com.example.hello.mapper")
+@RestController
 public class HelloApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "你好！这是我的第一个改动 🎉";
     }
 
 }
